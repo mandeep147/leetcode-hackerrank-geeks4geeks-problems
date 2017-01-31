449de2f1b60f00fdc52a9a21f431cbbbb0b210b7
mandeep kaur <mandeep kaur>
2017-01-31 15:43:23 -0800

    30 days of Code

---
+++ src/mandeep/hackerrank/thirtyDaysOfCode/RegExPatterns.java
@@ -0,0 +1,14 @@
+package mandeep.hackerrank.thirtyDaysOfCode;
+
+import java.util.Scanner;
+
+public class RegExPatterns {
+	 public static void main(String[] args) {
+	        Scanner in = new Scanner(System.in);
+	        int N = in.nextInt();
+	        for(int i = 0; i < N; i++){
+	            String firstName = in.next();
+	            String emailID = in.next();
+	        }
+	    }	
+}
