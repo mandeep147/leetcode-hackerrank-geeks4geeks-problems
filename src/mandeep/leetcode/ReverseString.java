83aa0926d16916ec0b88357d97687f9958c5eca5
mandeep147 <mndpkaur14@gmail.com>
2017-11-07 15:20:31 -0800

    reverse words in a string

--- src/mandeep/leetcode/ReverseString.java
+++ src/mandeep/leetcode/ReverseString.java
@@ -16,9 +16,29 @@ public class ReverseString {
        return reverseString;
     }
     
+    public static void reverseWords(String s) {
+        String[] words = s.split(" ");
+        int i = 0; 
+        String temp;
+        int len = words.length -1;
+        for(String str : words)
+        	System.out.println(str);
+        while(i < len){
+          temp = words[i];
+          words[i] = words[len];
+          words[len] = temp;
+          i++;
+          len--;   
+        }
+        
+        for(String str : words)
+        	System.out.println(str);
+    }
+    
     public static void main(String args[]){
     	@SuppressWarnings("unused")
 		String s = new ReverseString().reverseString("Hello");
     //	System.out.println();
+    	reverseWords("coding is fun");
     }
 }
