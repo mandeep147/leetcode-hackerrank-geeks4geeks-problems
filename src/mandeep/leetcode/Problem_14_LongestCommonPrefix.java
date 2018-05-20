941256a0258fd482da8002fdf6f4a0b568571437
mandeep147 <mndpkaur14@gmail.com>
2018-05-20 16:49:26 -0700

    Longest Common Prefix

---
+++ src/mandeep/leetcode/Problem_14_LongestCommonPrefix.java
@@ -0,0 +1,64 @@
+/**
+ * Problem 14: Longest Common Prefix
+ */
+package mandeep.leetcode;
+
+/**
+ * @author mandeep
+ * created on May 18, 2018, 9:56:49 AM
+ */
+public class Problem_14_LongestCommonPrefix {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String[] str = {"leetcode", "leet","lee","le"};
+		System.out.println(longestCommonPrefix(str));
+		System.out.println(longestCommonPrefixVertical(str));
+
+	}
+	
+	//===================Method -2 Vertical Scan=========================//
+	/**
+	 * Time: O(S)
+	 * S: sum of all characters in in all strings
+	 * Space: O(1)
+	 * @param str
+	 * @return
+	 */
+	private static String longestCommonPrefixVertical(String[] str) {
+		if(str == null || str.length == 0)
+			return "";
+		for(int i = 0; i < str[0].length(); i++){
+			char ch = str[0].charAt(i);
+			for(int j = 1; j < str.length; j++){
+				if(i == str[j].length() || ch !=str[j].charAt(i))
+					return str[0].substring(0, i);
+			}
+		}
+		return str[0];
+	}
+
+	//===================Method -1 Horizaontal Scan=========================//
+	/**
+	 * Time: O(S)
+	 * S: sum of all characters in in all strings
+	 * Space: O(1)
+	 * @param str
+	 * @return
+	 */
+	private static String longestCommonPrefix(String[] str) {
+		if(str.length == 0)
+			return "";
+		String prefix = str[0];
+		for(int i = 0; i < str.length; i++){
+			while(str[i].indexOf(prefix) != 0){
+				prefix = prefix.substring(0, prefix.length()-1);
+				if(prefix.isEmpty())
+					return "";
+			}
+		}
+		return prefix;
+	}
+}
